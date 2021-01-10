4bd40376c3d0b394d1545de11adcd1279242986a
trifan raul <raul.trifan@yahoo.com>
2021-01-10 21:22:08 +0200

    Add project files.

---
+++ Data/TRIFAN_RAUL_LAB6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TRIFAN_RAUL_LAB6.Models;
+
+namespace TRIFAN_RAUL_LAB6.Data
+{
+    public class TRIFAN_RAUL_LAB6Context : DbContext
+    {
+        public TRIFAN_RAUL_LAB6Context (DbContextOptions<TRIFAN_RAUL_LAB6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TRIFAN_RAUL_LAB6.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TRIFAN_RAUL_LAB6.Data;
+using TRIFAN_RAUL_LAB6.Models;
+
+namespace TRIFAN_RAUL_LAB6.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TRIFAN_RAUL_LAB6.Data.TRIFAN_RAUL_LAB6Context _context;
+
+        public IndexModel(TRIFAN_RAUL_LAB6.Data.TRIFAN_RAUL_LAB6Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
